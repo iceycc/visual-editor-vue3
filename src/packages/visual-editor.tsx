884aa3c8fc05d34e2f81b8237df95209d852f234
iceyang <w@icey.cc>
2021-01-24 11:04:22 +0800

    submit:主页面结构：左侧可选组件列表、中间容器画布、右侧编辑组件定义好的属性
    Signed-off-by: iceyang <w@icey.cc>

--- src/packages/visual-editor.tsx
+++ src/packages/visual-editor.tsx
@@ -1,13 +1,25 @@
-import { defineComponent } from 'vue'
-export const VisualEditor = defineComponent({
-    props:{
+import {defineComponent} from 'vue'
+import './visual-editor.scss'
 
-    },
-    setup(props){
-        return ()=> (
-            <div>
-                VisualEditor
+export const VisualEditor = defineComponent({
+    props: {},
+    setup(props) {
+        console.log(props)
+        return () => <div class="visual-editor">
+            <div class="visual-editor-menu">
+                visual-editor-menu
             </div>
-        )
+            <div class="visual-editor-head">
+                visual-editor-head
+            </div>
+            <div class="visual-editor-operator">
+                visual-editor-operator
+            </div>
+            <div class="visual-editor-body">
+                <div class="visual-editor-content">
+                    visual-editor-body
+                </div>
+            </div>
+        </div>
     }
 })
